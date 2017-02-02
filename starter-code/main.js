65627d588862184c572f8f4c638afa7c866cfe87
Shue Ze <d.dorkblue@gmail.com>
2017-02-02 13:03:26 +0800

    added expression to compare cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,28 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo){
+	alert("You've found a match!");
+}
+
+if (cardThree === cardFour){
+	alert("You've found a match!");
+}
+
+if (cardOne !== cardFour){
+	alert("Sorry, try again.");
+}
+
+if (cardOne !== cardThree){
+	alert("Sorry, try again.");
+}
+
+if (cardTwo !== cardFour){
+	alert("Sorry, try again.");
+}
+
+if (cardTwo !== cardThree){
+	alert("Sorry, try again.");
+}
+
