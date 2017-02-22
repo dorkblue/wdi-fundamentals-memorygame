132c3d7b5f4e04773f92777d6fcd6e502f8a4b97
Shue Ze <d.dorkblue@gmail.com>
2017-02-22 18:33:44 +0800

    added function for game logic. added start button to facilitate game start and replay.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,42 +1,66 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen','queen','king','king'];
+
+var cardsInPlay = [];
+
+var reset = function(){
+
+	cardsInPlay = [];
+
+	document.getElementById('game-board').textContent = "";
+
+	createCards();
+};
+
+document.getElementById('play').addEventListener('click', reset);
+
+
 
 var gameBoard = document.getElementById('game-board');
 
-var createCards = function (noOfCards){
-  for(var i = 1; i <= noOfCards; i++){
+var createCards = function (){
+
+  for(var i = 0; i < cards.length; i++){
+
     var cardCreated = document.createElement('div');
-    cardCreated.setAttribute('class', 'card');
+
+	cardCreated.setAttribute('class', 'card'); 
+    cardCreated.setAttribute('data-cards', cards[i]);
+
+    cardCreated.addEventListener('click', isTwoCards);
 
     document.getElementById('game-board').appendChild(cardCreated);
   };
 };
 
-/*if (cardOne === cardTwo){
-	alert("You've found a match!");
+var isTwoCards = function(){
+
+	if(this.getAttribute('data-cards') === 'queen'){
+		this.innerHTML = '<img class = "cardPic" src = "queen-card.png" alt = "Queen">';
+	}
+	else {
+		this.innerHTML = '<img class = "cardPic" src = "king-card.png" alt = "King">';	
+	}
+
+	cardsInPlay.push(this.getAttribute('data-cards'));
+
+	if (cardsInPlay.length === 2){
+
+		isMatch(cardsInPlay);
+
+		cardsInPlay = [];
+	}
 }
 
-if (cardThree === cardFour){
-	alert("You've found a match!");
+var isMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You've found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+
+	document.getElementById('play').textContent = "Play Again!";
+
 }
-
-if (cardOne !== cardFour){
-	alert("Sorry, try again.");
-}
-
-if (cardOne !== cardThree){
-	alert("Sorry, try again.");
-}
-
-if (cardTwo !== cardFour){
-	alert("Sorry, try again.");
-}
-
-if (cardTwo !== cardThree){
-	alert("Sorry, try again.");
-}*/
-
