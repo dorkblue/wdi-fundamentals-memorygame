e2eade1e663dc4c162dc25c082123c46701aa13e
Shue Ze <d.dorkblue@gmail.com>
2017-02-23 17:25:16 +0800

    added Fisher-Yates Shuffle to randomize cards array seq,

--- starter-code/main.js
+++ starter-code/main.js
@@ -10,12 +10,31 @@ var reset = function(){
 
 	document.getElementById('game-board').textContent = "";
 
+	cards = shuffle(cards);
+
 	createCards();
 };
 
 document.getElementById('play').addEventListener('click', reset);
 
+function shuffle(array) { //copied from http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 var gameBoard = document.getElementById('game-board');
 
